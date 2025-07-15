41d7e75326a7d3c321d75dc28aad053d46f60fda
eannaburke1 <74548665+eannaburke1@users.noreply.github.com>
2025-07-15 17:37:13 +0100

    Mongo connection details

--- CRUD/backend/index.js
+++ CRUD/backend/index.js
@@ -2,6 +2,43 @@ const express = require('express');
 const app = express();
 const data=require('./data/users.json');
 
+const { MongoClient, ServerApiVersion, ObjectId  } = require('mongodb');
+const uri = "mongodb+srv://applicationUser:KidsInTech16-07-25@cluster0.tp0a6zj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const databaseName = "kit";
+const collectionName = "users";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function findUserById(id) {
+  try {
+    await client.connect();
+    const database = client.db(databaseName); // Replace with your database name
+    const users = database.collection(collectionName);
+
+    // Convert string id to ObjectId
+    const objectId = new ObjectId(id);
+
+    // Query the user by _id
+    const user = await users.findOne({ _id: objectId });
+
+    console.log(user);
+    return user;
+  } catch (error) {
+    console.error('Error finding user:', error);
+  } finally {
+    await client.close();
+  }
+}
+
+findUserById("68768142bb3eafea61a3fff5").catch(console.dir);
+
 app.use(express.json());
 
 app.get('/', (request, response) => {
